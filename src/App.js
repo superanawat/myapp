c11574c8b8695ee026183dd6845f638b5f7ea360
superanawat <35832942+superanawat@users.noreply.github.com>
2023-03-01 13:21:28 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <div>This is React web</div>;
+  }
+}
+
+export default App;
